61a917cae108a68f60e0d5aa2009d05932ba3eae
wisdomwest <harmonykoome@gmail.com>
2024-03-29 15:10:41 +0300

    task 2 tricks but page_size was tricky

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -44,3 +44,26 @@ class Server:
         end = range[1]
 
         return self.dataset()[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        '''Implement a get_hyper method that takes same arguments as get_page
+        and returns a dictionary containing the following key-value pairs:
+        page_size: the length of the returned dataset page
+        page: the current page number
+        data: the dataset page (equivalent to return from previous task)
+        next_page: number of the next page, None if no next page
+        prev_page: number of the previous page, None if no previous page
+        total_pages: the total number of pages in the dataset as an integer'''
+
+        data = self.get_page(page, page_size)
+        total_pages = len(self.dataset()) / page_size
+        if total_pages % 1 != 0:
+            total_pages = int(total_pages) + 1
+        return {
+            'page_size': len(data),
+            'page': page,
+            'data': data,
+            'next_page': page + 1 if page < total_pages else None,
+            'prev_page': page - 1 if page > 1 else None,
+            'total_pages': int(total_pages)
+        }
