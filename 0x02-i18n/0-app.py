5491418733f0d260bae31d069e24f1c5a299891e
wisdomwest <harmonykoome@gmail.com>
2024-04-09 16:09:21 +0300

    lets see

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+''' Basic Flask app '''
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    ''' Home route '''
+    return render_template('0-index.html')
---
+++ 0x02-i18n/README.md
@@ -0,0 +1,1 @@
+Adding lang support flask
