2ec987b7431d0300f01551de021b1b72be863168
wisdomwest <harmonykoome@gmail.com>
2024-04-09 22:34:14 +0300

    chnages

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -33,7 +33,7 @@ def get_locale() -> str:
 @app.route('/', strict_slashes=False)
 def hello() -> str:
     """ Hello world """
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == "__main__":
