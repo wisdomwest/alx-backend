e1cf36fefcf186446059427ba870e6c9e7824d20
wisdomwest <harmonykoome@gmail.com>
2024-05-07 18:12:27 +0300

    task 3

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,30 @@
+import { createClient, print } from 'redis';
+import { promisify } from 'util';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err.message}`);
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, (err, reply) => {
+    print(`Reply: ${reply}`);
+  });
+}
+
+async function displaySchoolValue(schoolName) {
+  const getAsync = promisify(client.get).bind(client);
+  const reply = await getAsync(schoolName);
+  print(reply);
+}
+
+(async () => {
+  await displaySchoolValue('Holberton');
+  setNewSchool('HolbertonSanFrancisco', '100');
+  await displaySchoolValue('HolbertonSanFrancisco');
+})();
